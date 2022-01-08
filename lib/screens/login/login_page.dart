37ca4c233bd20766a9b09af380779ff8e112ed06
saheer <saheer.kp@mresult.com>
2022-01-08 20:57:25 +0530

    changing sign in page  1 . added validators for phone number

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -3,15 +3,14 @@ import 'package:flutter_application_1/constants/colors.dart';
 import 'package:flutter_application_1/screens/signup/signup.dart';
 import 'package:flutter_application_1/services/auth.dart';
 
-
 class LoginPage extends StatefulWidget {
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   final AuthServie _auth = AuthServie();
+  final _formKey = GlobalKey<FormState>();
   String email = "";
   String password = "";
   @override
@@ -20,27 +19,34 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              loginPageColorTop,
-              loginPageColorMid,
-              loginPageColorBottom              
-            ]
-          )
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            loginPageColorTop,
+            loginPageColorMid,
+            loginPageColorBottom
+          ]),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 80,),
+            SizedBox(
+              height: 80,
+            ),
             Padding(
               padding: EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),
-                  SizedBox(height: 10,),
-                  Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),),
+                  Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Welcome Back",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
                 ],
               ),
             ),
@@ -48,135 +54,182 @@ class _LoginPageState extends State<LoginPage> {
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
                 child: SingleChildScrollView(
                   child: Padding(
                     padding: EdgeInsets.all(30),
                     child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 30,),
+                      children: [
+                        SizedBox(height: 30),
                         Container(
                           decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [BoxShadow(
-                              color: Color.fromRGBO(225, 95, 27, .3),
-                              blurRadius: 20,
-                              offset: Offset(0, 10)
-                            )]
-                          ),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey))
-                                ),
-                                child: TextFormField(
-                                  onChanged: (val){
-                                      setState(()=> email =val);
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Color.fromRGBO(225, 95, 27, .3),
+                                    blurRadius: 20,
+                                    offset: Offset(0, 10))
+                              ]),
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom:
+                                              BorderSide(color: Colors.grey))),
+                                  child: TextFormField(
+                                    onChanged: (val) {
+                                      setState(() => email = val.toString());
                                     },
-                                  decoration: InputDecoration(
-                                    hintText: "Email or Phone number",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
+                                    validator: (value) {
+                                      return  validator(value, "Phone number");
+                                    },
+                                    decoration: InputDecoration(
+                                        hintText: "Phone number",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey),
+                                        border: InputBorder.none),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey))
-                                ),
-                                child: TextFormField(
-                                  onChanged: (val){
-                                      setState(()=> password =val);
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom:
+                                              BorderSide(color: Colors.grey))),
+                                  child: TextFormField(
+                                    onChanged: (val) {
+                                      setState(() => password = val);
                                     },
-                                  decoration: InputDecoration(
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
+                                    decoration: InputDecoration(
+                                        hintText: "Password",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey),
+                                        border: InputBorder.none),
                                   ),
                                 ),
-                              ),
-                            
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                        SizedBox(height: 40,),
-                        Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
-                        SizedBox(height: 40,),
+                        SizedBox(height: 40),
                         GestureDetector(
-                          onTap: ()async{
-                              dynamic result = await _auth.signInUsingEmailPassword(email: email,password: password,context: context);
-                              print(result);
-                            },
+                          onTap: () async {
+                            if (_formKey.currentState!.validate()) {
+                              // dynamic result =
+                              //     await _auth.signInUsingEmailPassword(
+                              //         email: email,
+                              //         password: password,
+                              //         context: context);
+                              // print(result);
+                              print('ok');
+                            }
+                          },
                           child: Container(
                             height: 50,
                             margin: EdgeInsets.symmetric(horizontal: 50),
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Colors.orange[900]
-                            ),                            
-                              child: Center(                              
-                                child: Text(
-                                  "Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.orange[900]),
+                            child: Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
                               ),
-                           
+                            ),
                           ),
                         ),
-                        SizedBox(height: 20,),
-                        GestureDetector(
-                          onTap: (){
-                              Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                  builder: (context) => SignUp(),
-                                ),
-                              );
-                            },
-                          child: Container(
-                            child:Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children:[
-                                Text("Not a member?", style: TextStyle(color: Colors.black, ),),
-                                Text("Sign up", style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),),
-                                ],
-                              )
-                              ),
+                        Text(
+                          "Forgot Password?",
+                          style: TextStyle(color: Colors.grey),
                         ),
-                        
-
-                        
-                        SizedBox(height: 50,),
-                        Text("Continue with social media", style: TextStyle(color: Colors.grey),),
-                        SizedBox(height: 30,),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => SignUp(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                              child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Not a member?",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Text(
+                                "Sign up",
+                                style: TextStyle(
+                                    color: Colors.orange,
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic),
+                              ),
+                            ],
+                          )),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Text(
+                          "Continue with social media",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
                         Row(
                           children: <Widget>[
                             Expanded(
                               child: Container(
                                 height: 50,
                                 decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.blue
-                                ),
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.blue),
                                 child: Center(
-                                  child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                  child: Text(
+                                    "Facebook",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
                             ),
-                            SizedBox(width: 30,),
+                            SizedBox(
+                              width: 30,
+                            ),
                             Expanded(
                               child: Container(
                                 height: 50,
                                 decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.red
-                                ),
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.red),
                                 child: Center(
-                                  child: Text("Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-
+                                  child: Text(
+                                    "Google",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
                             )
@@ -194,6 +247,23 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  String? validator(value,from) {
+    if (value == null || value.isEmpty) {
+      return "Please enter $from";
+    }else{
+    if(from! =="Phone number"){
 
+      if (double.tryParse(value)==null){
+        return "Phone number is not valid, please try again";
+      }
+      else {
+        if (value.length !=10){
+          return "Please enter a 10 digit Phone number";
+        }
+      }
+    }
+    
+    }
+
+  }
 }
-
